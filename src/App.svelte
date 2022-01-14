1033e8b5790911b3fc273ee64a1ae999c6a350d1
Nikita Gaidakov <ngaidakov@podfather.com>
2022-01-14 23:52:06 +0000

    Setup Svelte. Create first component.

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<p>
+  The <span on:click={ shuffle }><i>{ word }</i></span> making known
+</p>
+
+<script>
+const words = [
+  'real',
+  'incredible',
+  'unbelievable',
+  'inexorable',
+  'indefensible',
+]
+
+let word = words[0]
+
+function shuffle () {
+  const i = Math.floor(Math.random() * words.length)
+  word = words[i]
+}
+</script>
