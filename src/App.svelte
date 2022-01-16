30bd9871262e4f196b1dcd818b32bad0fb19302f
Nikita Gaidakov <ngaidakov@podfather.com>
2022-01-16 23:06:23 +0000

    Try using inview directly on children of Carousel

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
 import Carousel from './Carousel.svelte'
+import { inview } from 'svelte-inview'
+
+let view: 'header' | 'menu' = 'header'
 </script>
 
-<Carousel orientation="vertical">
-  <header>
+<Carousel orientation="vertical" class={view}>
+  <header use:inview={{ threshold: 0.5 }} on:enter={() => view = 'header'}>
     <h1>The Making Known</h1>
   </header>
 
-  <div>
+  <div use:inview={{ threshold: 0.5 }} on:enter={() => view = 'menu'}>
     <p>This is what I think of that</p>
   </div>
 </Carousel>
 
 <style>
+:global(body:has(.header)) {
+  background: linear-gradient(to bottom, #302d2c, #6e7172 50%);
+}
+
+:global(body:has(.menu)) {
+  background: #a6241d;
+}
+
 main code {
   background: #a6241d;
   padding: 4px 8px;
