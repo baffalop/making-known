9db55c70edc60a7bdd985f5d1eb1cc1025c3e85d
Nikita Gaidakov <ngaidakov@podfather.com>
2022-02-16 03:16:34 +0000

    Place images for menu items

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -10,9 +10,11 @@ function select () {
 </script>
 
 <ul>
-  {#each [Piece.Jane, Piece.Dianna, Piece.Paul] as piece, i}
-    <li style="--i: {i}">
-      <a href="#{piece}" on:click={select}>No. {i + 1} {titleFor(piece)}</a>
+  {#each [Piece.Jane, Piece.Dianna, Piece.Paul] as piece}
+    <li>
+      <a href="#{piece}" on:click={select}>
+        <img src="img/item-{piece}.png" alt={titleFor(piece)} class={piece} />
+      </a>
     </li>
   {/each}
 </ul>
@@ -20,10 +22,36 @@ function select () {
 <style>
 ul {
   list-style: none;
-  line-height: 2em;
+  line-height: 4em;
 }
 
-li {
-  transform: translateX(calc((var(--i) - 1) * 0.6em));
+img {
+  height: 2.5em;
+}
+
+a {
+  -webkit-tap-highlight-color: transparent;
+  transition: opacity 120ms ease-in 150ms;
+}
+
+a:active {
+  transition: none;
+  opacity: 0.5;
+}
+
+.jane {
+  margin-left: -1.4em;
+  height: 2.9em;
+}
+
+.dianna {
+  margin-left: -0.6em;
+  height: 2.7em;
+}
+
+.paul {
+  margin-top: 0.4em;
+  margin-left: 1.4em;
+  height: 2.6em;
 }
 </style>
