3cd4c11806c819c567527c6d1de997a35ba2d682
Nikita Gaidakov <ngaidakov@podfather.com>
2022-02-16 02:09:53 +0000

    Change base back to /

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ import tsconfig from './tsconfig.json';
 
 const production = process.env.NODE_ENV === 'production';
 const config = <UserConfig> defineConfig({
-	base: './',
+	base: '/',
 	plugins: [
 		svelte({
 			emitCss: production,
